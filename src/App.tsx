16fd65e948b47454fbc9b63a46e021c1163b04d8
Misterleo <misterleo.minecraft@gmail.com>
2026-01-29 19:47:16 +0100

    creazione base

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+      <div>
+        <ul className="flex justify-center gap-7 mt-6">
+          <li>Chi sono</li>
+          <li>Lavoro</li>
+          <li>Hobby</li>
+        </ul>
+      </div>
+  )
+}
+
+export default App
