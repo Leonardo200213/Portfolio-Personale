d9700bfea64d0afd42bcaf747c81b2b9c94198d5
Misterleo <misterleo.minecraft@gmail.com>
2026-02-07 15:08:40 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-function App() {
+import { BrowserRouter, NavLink } from "react-router-dom"
 
+function App() {
   return (
-      <div>
-        <ul className="flex justify-center gap-7 mt-6">
-          <li>Chi sono</li>
-          <li>Lavoro</li>
-          <li>Hobby</li>
+    <BrowserRouter>
+      <div >
+        <ul className="flex mx-auto gap-5 mt-6 font-bold text-lg border border-gray-500 p-2.5 rounded-full w-fit shadow-lg select-none">
+          <li>
+            <NavLink
+              to="/home"
+              className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+              Home
+            </NavLink>
+          </li>
+           <li>
+            <NavLink
+              to="/work"
+              className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+              Lavoro
+            </NavLink>
+          </li>
+           <li>
+            <NavLink
+              to="/hobby"
+              className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+            >
+              Hobby
+            </NavLink>
+          </li>
         </ul>
       </div>
+    </BrowserRouter>
+
+      
   )
 }
 
